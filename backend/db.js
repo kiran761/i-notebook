e55f6cc57990074dd7f89716054cdc1a1e9780fa
Kiran Kumar Reddy <rkirankumar761@gmail.com>
2024-02-22 19:57:32 +0530

    Some Backend Changes

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// connectToMongo().catch(err => console.log(err));
+
+const connectToMongo= () => {
+   mongoose.connect('mongodb://localhost:27017');
+  console.log("Connected Successfully");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+module.exports = connectToMongo;
