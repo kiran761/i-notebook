090995e0ab996e5ee4e3f82d93420c5af7e7209b
Kiran Kumar Reddy <rkirankumar761@gmail.com>
2024-02-27 10:40:32 +0530

    Inserting data into MongoDB

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // connectToMongo().catch(err => console.log(err));
 
 const connectToMongo= () => {
-   mongoose.connect('mongodb://localhost:27017');
+   mongoose.connect('mongodb://localhost:27017/iNoteBook');
   console.log("Connected Successfully");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-
+const { body, validationResult } = require('express-validator')
 
 //Create a user using post '/api/auth/" . Doesnt require auth"
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user=User(req.body);
-    user.save()
-    res.send(req.body);
+router.post('/createuser', [
+    body('email', 'Enter a Valid Email').isEmail(),
+    body('name', 'Enter a valid email').isLength({ min: 3 }),
+    body('password', 'password atleast should be 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    // if there are errors return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    let user = await User.findOne({ email: req.body.email });
+    try {
+        if (user) {
+            return res.status(400).json({ error: " This Email is already used" })
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
 
+        res.json(user)
+    } catch (error) {
+        Console.log(error.message);
+        res.status(500).send("Some Error Occured")
+    }
 })
 
 module.exports = router
