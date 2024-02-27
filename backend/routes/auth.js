cb725db647228e3d6fba36d18e7ebf121b30fdb6
Kiran Kumar Reddy <rkirankumar761@gmail.com>
2024-02-27 15:24:22 +0530

    bcrypt password and sent auth tokrn as response for correct entry

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs');
+const jwt=require('jsonwebtoken')
 
+const JWT_SECRET = "Kiran Kumar Reddy";
 //Create a user using post '/api/auth/" . Doesnt require auth"
 router.post('/createuser', [
     body('email', 'Enter a Valid Email').isEmail(),
@@ -19,13 +22,20 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: " This Email is already used" })
         }
+        const salt=await bcrypt.genSalt(10);
+        const secPassword=await bcrypt.hash(req.body.password,salt);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPassword
         })
-
-        res.json(user)
+        const data ={
+            user :{
+                id :user.id
+            }
+        }
+        const authToken=jwt.sign(data,JWT_SECRET);
+        res.json(authToken)
     } catch (error) {
         Console.log(error.message);
         res.status(500).send("Some Error Occured")
