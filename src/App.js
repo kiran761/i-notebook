4ac567f96a9a820c28f74e100a7b7000833e0d30
Kiran Kumar Reddy <rkirankumar761@gmail.com>
2024-02-29 12:42:51 +0530

    Added Navbar and routes for Home and About Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import About from './components/About'
+import Home from './components/Home'
+import { HashRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+        < Route exact path="/Home" element={<Home />} />
+          < Route exact path="/About" element={<About />} />
+        </Routes>
+      </HashRouter>
 
     </>
 
