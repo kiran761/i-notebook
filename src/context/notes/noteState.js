fc6e74f3ca7a42f0bac35087c0ec7a49a888e6b4
Kiran Kumar Reddy <rkirankumar761@gmail.com>
2024-03-02 11:29:15 +0530

    Created notes and noteItem components

--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -20,7 +20,43 @@ const NoteState=(props)=>{
           "tag": "LeetCode",
           "date": "2024-02-28T09:34:17.280Z",
           "__v": 0
-        }
+        },
+        {
+          "_id": "65decefb31da8077af8d42ce",
+          "user": "65ddb5d2452a3dcd0f8b3986",
+          "title": "Backend",
+          "description": " Web Development Course",
+          "tag": "Youtube",
+          "date": "2024-02-28T06:13:15.498Z",
+          "__v": 0
+        },
+        {
+          "_id": "65defe193f7f2d4bd63e11ca",
+          "user": "65ddb5d2452a3dcd0f8b3986",
+          "title": "DSA",
+          "description": " Striver DSA Course",
+          "tag": "LeetCode",
+          "date": "2024-02-28T09:34:17.280Z",
+          "__v": 0
+        },
+        {
+          "_id": "65decefb31da8077af8d42ce",
+          "user": "65ddb5d2452a3dcd0f8b3986",
+          "title": "Backend",
+          "description": " Web Development Course",
+          "tag": "Youtube",
+          "date": "2024-02-28T06:13:15.498Z",
+          "__v": 0
+        },
+        {
+          "_id": "65defe193f7f2d4bd63e11ca",
+          "user": "65ddb5d2452a3dcd0f8b3986",
+          "title": "DSA",
+          "description": " Striver DSA Course",
+          "tag": "LeetCode",
+          "date": "2024-02-28T09:34:17.280Z",
+          "__v": 0
+        },
       ]
 
       const [notes,setNotes]=useState(notesInitial)
